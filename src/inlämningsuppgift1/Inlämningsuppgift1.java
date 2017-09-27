0241c8882e69c6f4a0f833a9193a2586eddfc627
Kevin Engvall <kevin.engvall@gmail.com>
2017-09-27 14:34:09 +0200

    Mitt projekts första committ

---
+++ src/inlämningsuppgift1/Inlämningsuppgift1.java
@@ -0,0 +1,14 @@
+
+package inlämningsuppgift1;
+
+
+public class Inlämningsuppgift1 {
+
+    
+    public static void main(String[] args) {
+       
+        System.out.println("Hello World!");
+        
+    }
+    
+}
